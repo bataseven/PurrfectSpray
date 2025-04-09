90b3bd8548be67064235317fcda541c31073aa28
bataseven <bataseven15@ku.edu.tr>
2025-04-09 23:04:27 +0300

    Added a calibration script saves pixels and motor angles

--- Code/hardware.py
+++ Code/hardware.py
@@ -1,14 +1,11 @@
+# hardware.py
 from gpiozero import DigitalInputDevice, OutputDevice
 
-# Hall effect sensors
 hall_sensor_1 = DigitalInputDevice(6, pull_up=False)
 hall_sensor_2 = DigitalInputDevice(26, pull_up=False)
-
-# Actuators
+fan_pin = OutputDevice(11, active_high=True, initial_value=True)
 laser_pin = OutputDevice(0, active_high=True, initial_value=False)
 water_gun_pin = OutputDevice(1, active_high=True, initial_value=False)
-fan_pin = OutputDevice(11, active_high=True, initial_value=True)
 
-# Stepper enable pins
 enable_pin_1 = OutputDevice(12, active_high=False, initial_value=False)
 enable_pin_2 = OutputDevice(4, active_high=False, initial_value=False)
