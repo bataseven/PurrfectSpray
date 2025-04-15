b0231b3bac70c6aa1b4ac2217e5cf087076bf3f9
bataseven <bataseven15@ku.edu.tr>
2025-04-15 21:43:32 +0300

    optimizing for multiple clients

---
+++ Code/app_globals.py
@@ -0,0 +1,20 @@
+# app_globals.py
+from threading import Event, Lock
+from flask_socketio import SocketIO
+
+socketio : SocketIO = None 
+
+# Auto-tracking state
+auto_mode = False
+tracking_target = "person"
+
+# Target for motor movement
+latest_target_coords = (None, None)
+target_lock = Event()
+
+viewer_count = 0
+# Lock for viewer count / controller access
+viewer_lock = Lock()
+
+# The client that is currently controlling the motors
+active_controller_sid = None
